b121dac3da7fc73c9ed35f79f2fc614ce816c90a
koganei <khoury.marc@gmail.com>
2014-06-15 17:05:31 -0400

    Added directive and readme

---
+++ Readme.md
@@ -0,0 +1,11 @@
+# Angular Directive: onSelected
+
+Super easy to implement
+
+    <div on-selected="myFunction"></div>
+
+Then in your controller
+    
+    scope.myFunction = function(word, selection) {};
+
+Got the basic selection code from http://stackoverflow.com/a/10180367
---
+++ onSelected.js
@@ -0,0 +1,52 @@
+angular.module('koganei.onSelected').
+directive('onSelected', [function() {
+
+    function getSelected() {
+        var text = "";
+        if (window.getSelection
+        && window.getSelection().toString()
+        && $(window.getSelection()).attr('type') != "Caret") {
+            text = window.getSelection();
+            return text;
+        }
+        else if (document.getSelection
+        && document.getSelection().toString()
+        && $(document.getSelection()).attr('type') != "Caret") {
+            text = document.getSelection();
+            return text;
+        }
+        else {
+            var selection = document.selection && document.selection.createRange();
+
+            if (!(typeof selection === "undefined")
+            && selection.text
+            && selection.text.toString()) {
+                text = selection.text;
+                return text;
+            }
+        }
+
+        return false;
+    }
+
+    return {
+        restrict: 'A',
+
+        replace: false,
+
+        link: function(scope, element, attrs) {
+          element.bind('mouseup', function() {
+            if(angular.isFunction(scope.onSelected)) {
+
+              var selected = getSelected();
+              window.selected = selected;
+              scope.onSelected.apply(this, [selected.toString(), selected]);
+            }
+          });
+
+        },
+        scope: {
+          onSelected: '='
+        }
+      };
+  }]);
